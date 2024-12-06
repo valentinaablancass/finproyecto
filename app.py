6d04d343578aa9ca06cdbd03d611e79d52d898e8
valentinaablancass <valentina.blancas@ciencias.unam.mx>
2024-12-05 22:59:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -727,7 +727,7 @@ with tab3:
             cols = st.columns(3)
             for col, (label, value) in zip(cols, list(stats.items())[i:i+3]):
                 with col:
-                    st.markdown(render_metric(label, f"{value:.2f}", background_color="#da4167", border_left_color="#1b065e"), unsafe_allow_html=True)
+                    st.markdown(render_metric(label, f"{value:.2f}", background_color="#da4167", border_left_color="#7192be"), unsafe_allow_html=True)
 
     # Columna 2: Mínima Volatilidad (Rendimiento 10%) y Pesos Iguales (en 3 boxes por fila cada uno)
     with col2:
